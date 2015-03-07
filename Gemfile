d1a156b443c46a04f17cb882f7ddeef1eb0e9e6c
mohamehdi <maourir@college.harvard.edu>
2015-03-07 17:53:46 -0500

    we added users to the tweets and did authentication stuff

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem to use form
 gem 'simple_form'
 
+# user registration 
+gem 'devise'
+
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,8 @@
 class TweetsController < ApplicationController
+	
+	# To make sure the user is signed in first! 
+	before_action :authenticate_user!
+
 	def new
 		# Create a new instance of the class Tweet that we just created as a model
 		@tweet = Tweet.new 
@@ -7,12 +11,22 @@ class TweetsController < ApplicationController
 	def create
 		# This @tweet has no idea that the other @tweet in the othe action. THEY ARE DIFFRENT AND HAVE NOTHING TO DO WITH EACH OTHER!!!! 
 		@tweet = Tweet.new(tweet_params)
+		@tweet.user = current_user
+		if @tweet.save
+			flash[:success] = "You have created a tweet!"
+			# render vs redirect ? 
+			redirect_to new_tweet_path
+		end
 		@tweet.save 
-		render 'new'
+		# render 'new'
 	end
 
 	def tweet_params
 		params.require(:tweet).permit(:content)
+	end
+
+	def index 
+		@tweets = Tweet.all
 	end
 
 end

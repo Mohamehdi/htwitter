dc73495e915e834242e2ce1bf89b049446872b9c
mohamehdi <maourir@college.harvard.edu>
2015-03-07 14:06:44 -0500

    made tweets create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,16 @@ class TweetsController < ApplicationController
 		# Create a new instance of the class Tweet that we just created as a model
 		@tweet = Tweet.new 
 	end
+
+	def create
+		# This @tweet has no idea that the other @tweet in the othe action. THEY ARE DIFFRENT AND HAVE NOTHING TO DO WITH EACH OTHER!!!! 
+		@tweet = Tweet.new(tweet_params)
+		@tweet.save 
+		render 'new'
+	end
+
+	def tweet_params
+		params.require(:tweet).permit(:content)
+	end
+
 end
